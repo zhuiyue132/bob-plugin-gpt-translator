a76c88417889044ed7834ce86731ea5617961421
zhuiyue132 <fenda.app@qq.com>
2023-06-08 18:20:40 +0800

    feat(init): first commit;

---
+++ README.md
@@ -0,0 +1,33 @@
+<div>
+  <h1 align="center">GPT Translator Plugin For Bob </h1>
+</div>
+
+
+## 简介
+
+直接调用chatGPT网页端 而非 openai api 的 Bob 翻译插件。
+
+随意维护，随意更新，主要供自己使用。
+
+支持社区版 Bob, 商店版应该也支持。但我没试。
+
+## 打包方式
+
+![fiL6r2](https://picture.zhuiyue.vip:444/images/2023/06/08/fiL6r2.png)
+
+## 使用方式
+  0. 安装 Bob, 商店版和社区版都可以。
+  
+  1. 先部署[潘多拉](https://github.com/pengzhile/pandora/blob/master/doc/wiki.md), 得到服务的地址。
+  
+  2. 安装插件，按照上方打包方式自行打包，并安装之。
+
+  3. 把潘多拉服务器地址填到插件里，并启用插件。
+
+
+## 感谢
+  - Bob 作者 [@ripperhe](https://github.com/ripperhe)
+  - yetone.openai.translator 作者 [@yetone](https://github.com/yetone)
+  - 潘多拉作者 [@pengzhile](https://github.com/pengzhile)
+
+     排名不分先后。
---
+++ src/main.js
@@ -0,0 +1,278 @@
+//@ts-check
+
+var lang = require("./lang.js");
+var ChatGPTModels = ["text-davinci-002-render-sha"];
+
+var SYSTEM_PROMPT =
+  "You are a translation engine that can only translate text and cannot interpret it.";
+
+var HttpErrorCodes = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  402: "Payment Required",
+  403: "Forbidden",
+  404: "Not Found",
+  405: "Method Not Allowed",
+  406: "Not Acceptable",
+  407: "Proxy Authentication Required",
+  408: "Request Timeout",
+  409: "Conflict",
+  410: "Gone",
+  411: "Length Required",
+  412: "Precondition Failed",
+  413: "Payload Too Large",
+  414: "URI Too Long",
+  415: "Unsupported Media Type",
+  416: "Range Not Satisfiable",
+  417: "Expectation Failed",
+  418: "I'm a teapot",
+  421: "Misdirected Request",
+  422: "Unprocessable Entity",
+  423: "Locked",
+  424: "Failed Dependency",
+  425: "Too Early",
+  426: "Upgrade Required",
+  428: "Precondition Required",
+  429: "Too Many Requests",
+  431: "Request Header Fields Too Large",
+  451: "Unavailable For Legal Reasons",
+  500: "Internal Server Error",
+  501: "Not Implemented",
+  502: "Bad Gateway",
+  503: "Service Unavailable",
+  504: "Gateway Timeout",
+  505: "HTTP Version Not Supported",
+  506: "Variant Also Negotiates",
+  507: "Insufficient Storage",
+  508: "Loop Detected",
+  510: "Not Extended",
+  511: "Network Authentication Required",
+};
+
+/**
+ *  生成一个 uuid
+ * @returns
+ */
+function uuid() {
+  let d = new Date().getTime();
+  if (
+    typeof performance !== "undefined" &&
+    typeof performance.now === "function"
+  ) {
+    d += performance.now(); // 添加性能参数以提高唯一性
+  }
+  const uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+    /[xy]/g,
+    function (c) {
+      const r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+    }
+  );
+  return uuid;
+}
+
+/**
+ * @param {string}  url
+ * @returns {string}
+ */
+function ensureHttpsAndNoTrailingSlash(url) {
+  const hasProtocol = /^[a-z]+:\/\//i.test(url);
+  const modifiedUrl = hasProtocol ? url : "https://" + url;
+
+  return modifiedUrl.endsWith("/") ? modifiedUrl.slice(0, -1) : modifiedUrl;
+}
+
+/**
+ * The header object.
+ */
+function buildHeader() {
+  return {
+    "Content-Type": "application/json",
+  };
+}
+
+/**
+ * @param {Bob.TranslateQuery} query
+ * @returns {{
+ *  generatedSystemPrompt: string,
+ *  generatedUserPrompt: string
+ * }}
+ */
+function generatePrompts(query) {
+  let generatedSystemPrompt = SYSTEM_PROMPT;
+  const { detectFrom, detectTo } = query;
+  const sourceLang = lang.langMap.get(detectFrom) || detectFrom;
+  const targetLang = lang.langMap.get(detectTo) || detectTo;
+  let generatedUserPrompt = `translate from ${sourceLang} to ${targetLang}`;
+
+  if (detectTo === "wyw" || detectTo === "yue") {
+    generatedUserPrompt = `翻译成${targetLang}`;
+  }
+
+  if (
+    detectFrom === "wyw" ||
+    detectFrom === "zh-Hans" ||
+    detectFrom === "zh-Hant"
+  ) {
+    if (detectTo === "zh-Hant") {
+      generatedUserPrompt = "翻译成繁体白话文";
+    } else if (detectTo === "zh-Hans") {
+      generatedUserPrompt = "翻译成简体白话文";
+    } else if (detectTo === "yue") {
+      generatedUserPrompt = "翻译成粤语白话文";
+    }
+  }
+  if (detectFrom === detectTo) {
+    generatedSystemPrompt =
+      "You are a text embellisher, you can only embellish the text, don't interpret it.";
+    if (detectTo === "zh-Hant" || detectTo === "zh-Hans") {
+      generatedUserPrompt = "润色此句";
+    } else {
+      generatedUserPrompt = "polish this sentence";
+    }
+  }
+
+  generatedUserPrompt = `${generatedUserPrompt}:\n\n${query.text}`;
+
+  return { generatedSystemPrompt, generatedUserPrompt };
+}
+
+/**
+ * @param {string} prompt
+ * @param {Bob.TranslateQuery} query
+ * @returns {string}
+ */
+function replacePromptKeywords(prompt, query) {
+  if (!prompt) return prompt;
+  return prompt
+    .replace("$text", query.text)
+    .replace("$sourceLang", query.detectFrom)
+    .replace("$targetLang", query.detectTo);
+}
+
+/**
+ * @param {Bob.TranslateQuery} query
+ * @returns {{
+ *  model: typeof ChatGPTModels[number];
+ *  prompt?: string;
+ *  stream?: boolean;
+ * }}
+ */
+function buildRequestBody(query) {
+  let { customUserPrompt } = $option;
+  const { generatedUserPrompt } = generatePrompts(query);
+  customUserPrompt = replacePromptKeywords(customUserPrompt, query);
+
+  const userPrompt = `${SYSTEM_PROMPT}${
+    customUserPrompt || generatedUserPrompt
+  }`;
+
+  return {
+    stream: false,
+    model: ChatGPTModels[0],
+    prompt: userPrompt,
+  };
+}
+
+function handleError(completion, result) {
+  const { statusCode } = result.response;
+  const reason = statusCode >= 400 && statusCode < 500 ? "param" : "api";
+  completion({
+    error: {
+      type: reason,
+      message: `接口响应错误 - ${HttpErrorCodes[statusCode]}`,
+      addtion: `${JSON.stringify(result)}`,
+    },
+  });
+}
+
+/**
+ * @type {Bob.Translate}
+ */
+function translate(query, completion) {
+  if (!lang.langMap.get(query.detectTo)) {
+    completion({
+      error: {
+        type: "unsupportLanguage",
+        message: "不支持该语种",
+        addtion: "不支持该语种",
+      },
+    });
+  }
+
+  const { apiUrl } = $option;
+
+  if (!apiUrl) {
+    completion({
+      error: {
+        type: "secretKey",
+        message: "配置错误 - 请确保您在插件配置中填入了正确的 API URL",
+        addtion: "请在插件配置中填写 API URL",
+      },
+    });
+  }
+
+  const modifiedApiUrl = ensureHttpsAndNoTrailingSlash(apiUrl);
+
+  let apiUrlPath = "/api/conversation/talk";
+  let deleteUrl = `/api/conversation/`;
+
+  const header = buildHeader();
+  const body = buildRequestBody(query);
+
+  let targetText = ""; // 初始化拼接结果变量
+  let conversation_id = null;
+  (async () => {
+    await $http.request({
+      method: "POST",
+      url: modifiedApiUrl + apiUrlPath,
+      header,
+      body: { ...body, message_id: uuid(), parent_message_id: uuid() },
+      handler: async (result) => {
+        if (result.response.statusCode >= 400) {
+          handleError(completion, result);
+        } else {
+          $log.info(`result: ${JSON.stringify(result)}`);
+
+          targetText = result.data.message.content.parts[0];
+          conversation_id = result.data.conversation_id;
+          $log.info(`conversation_id: ${conversation_id}`);
+          completion({
+            result: {
+              from: query.detectFrom,
+              to: query.detectTo,
+              toParagraphs: [targetText],
+            },
+          });
+          // 删除对话
+          // 每次翻译都是新的对话，为了避免登录网页版一看列表全是翻译记录，影响正常使用。
+          // 所以选择每次翻译后删除对话。
+          await $http.request({
+            method: "DELETE",
+            url: modifiedApiUrl + deleteUrl + conversation_id,
+            header,
+            handler: (result) => {
+              $log.info(`result: ${JSON.stringify(result)}`);
+            },
+          });
+        }
+      },
+    });
+  })().catch((err) => {
+    completion({
+      error: {
+        type: err._type || "unknown",
+        message: err._message || "未知错误",
+        addtion: err._addition,
+      },
+    });
+  });
+}
+
+function supportLanguages() {
+  return lang.supportLanguages.map(([standardLang]) => standardLang);
+}
+
+exports.supportLanguages = supportLanguages;
+exports.translate = translate;
